8f17d626dc38a82862618468e2894197cfae2f10
charan <sravanamcharan20@gmail.com>
2025-02-05 12:42:06 +0530

    deployable changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       '/restaurants-by-cuisine': {
